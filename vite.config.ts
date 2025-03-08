2870b4df4488c0d4b7e878f5984f5ece583114f0
see518 <srk0876@gmail.com>
2025-03-08 15:17:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/trendifyhub3/", // Fix for GitHub Pages
   plugins: [react()],
+});
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
